457e0939076772fa1efee9b3fb04fe1bb8c4771e
Samar-al <samar.alkhalilchebaye@gmail.com>
2024-05-27 12:01:19 +0200

    initial commit

---
+++ demo3/src/script.js
@@ -0,0 +1,19 @@
+function init(){
+    const cities = ['Quimper', 'Brest', 'Concarneau', 'Toulouse'];
+
+    let select=document.getElementById("cities");
+    cities.forEach(function (val) {
+        let option = document.createElement('option');
+        option.value = val.toLowerCase();
+        option.innerText = val;
+        select.append(option)
+    });
+
+    select.addEventListener('change', function(){
+        let p = document.createElement('p');
+        p.innerText = this.value;
+        document.body.append(p);
+    })
+}
+
+window.onload = init;
